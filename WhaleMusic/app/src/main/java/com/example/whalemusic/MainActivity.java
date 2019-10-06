2922f384a2ae3a3024aca6f87d3df1f3ee3ff0ef
Georgina Da Luz <daluz.georgina@gmail.com>
2019-10-06 18:06:00 -0300

    Edite Main

--- WhaleMusic/app/src/main/java/com/example/whalemusic/MainActivity.java
+++ WhaleMusic/app/src/main/java/com/example/whalemusic/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //EDITE EL MAIN!
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

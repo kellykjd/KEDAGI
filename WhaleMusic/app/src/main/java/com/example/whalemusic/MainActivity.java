c7d3b8ccf199f22359247c6cb075541fb87c0ee5
Kelly Jimenez <kellykjd@gmail.com>
2019-10-07 13:21:57 -0300

    Se agrega: Main Fragment Main Activity Imágenes (temporales) mientras se definen las finales Se agrega la dependencia: implementation 'de.hdodenhof:circleimageview:2.2.0' para las imágenes circulares Se crea un estilo para los radioButtons de la selección de géneros

--- WhaleMusic/app/src/main/java/com/example/whalemusic/MainActivity.java
+++ WhaleMusic/app/src/main/java/com/example/whalemusic/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.whalemusic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainFragment.NotificadorActivity {
 
 
 
@@ -13,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pegarFragment(new MainFragment());
+
+    }
 
 
+    private void pegarFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainActivity_contenedorFragments,fragment,null)
+                .commit();
+    }
+
+    @Override
+    public void recibirGeneros(List<String> generos) {
+        //A la espera de Gigi para continuar.
     }
 }
---
+++ WhaleMusic/app/src/main/java/com/example/whalemusic/MainFragment.java
@@ -0,0 +1,105 @@
+package com.example.whalemusic;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    private CheckBox checkBoxRock;
+    private CheckBox checkBoxPop;
+    private CheckBox checkBoxIndie;
+    private CheckBox checkBoxReggae;
+    private CheckBox checkBoxCountry;
+    private CheckBox checkBoxMetal;
+    private Button buttonSkip;
+    private NotificadorActivity listener;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View viewMainFragment = inflater.inflate(R.layout.fragment_main, container, false);
+        encontrarVistas(viewMainFragment);
+
+        buttonSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.recibirGeneros(seleccionDeGeneros());
+            }
+        });
+
+
+
+
+
+
+
+
+
+        return viewMainFragment;
+    }
+
+    private void encontrarVistas(View view){
+        checkBoxRock = view.findViewById(R.id.MainFragment_checkBox_rock);
+        checkBoxPop = view.findViewById(R.id.MainFragment_checkBox_pop);
+        checkBoxIndie = view.findViewById(R.id.MainFragment_checkBox_indie);
+        checkBoxReggae = view.findViewById(R.id.MainFragment_checkBox_reggae);
+        checkBoxCountry = view.findViewById(R.id.MainFragment_checkBox_country);
+        checkBoxMetal = view.findViewById(R.id.MainFragment_checkBox_metal);
+        buttonSkip = view.findViewById(R.id.MainFragment_button_skip);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (NotificadorActivity) context;
+    }
+
+    public interface NotificadorActivity{
+        public void recibirGeneros(List<String> generos);
+    }
+
+    private List<String> seleccionDeGeneros(){
+        List<String> generos = new ArrayList<>();
+        if (checkBoxRock.isChecked()){
+            generos.add("Rock");
+        }
+        if (checkBoxPop.isChecked()){
+            generos.add("Pop");
+        }
+        if (checkBoxIndie.isChecked()){
+            generos.add("Indie");
+        }
+        if (checkBoxReggae.isChecked()){
+            generos.add("Reggae");
+        }
+        if (checkBoxCountry.isChecked()){
+            generos.add("Country");
+        }
+        if (checkBoxMetal.isChecked()){
+            generos.add("Metal");
+        }
+        return generos;
+    }
+}
